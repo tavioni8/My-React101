23a483dc1a17ff9ce4a0cc4e02524c58c67b1765
932412628 <tavionisean@gmail.com>
2020-07-24 16:15:49 +1200

    New Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.notes = [
+      {
+        id:1,
+        text: 'Watch The Godfather 1',
+      },
+      {
+        id:2,
+        text: 'Watch The Godfather 2',
+      },
+      {
+        id:3,
+        text: 'Watch The Godfather 3',
+      },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="wrap">
+          <div className="container">
+              <div className="tittle">
+                  <h1>Daily Todo List</h1>
+              </div>
+              <div className="add-note">
+                  <label htmlFor="note-input"></label>
+                  <input type="text" placeholder=" Add Your Todo" className="note-input"></input>
+                  <i className="fas fa-plus"></i>
+              </div>
+              <div className="notes">
+                {
+                  this.notes.map((note)=>{
+                    return (
+                      <div className="note n1">
+                          <label className="checkbox">
+                              <input type="checkbox" name="" id="checkbox"></input>
+                          </label>
+                          <div className="note-text">
+                          {note.text}
+                          </div>
+                      </div>
+                    )
+                  })
+                }  
+              </div>
+          </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
