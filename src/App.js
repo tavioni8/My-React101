2814258fa30026b18d811b87ddde1eb5c4f9638d
932412628 <tavionisean@gmail.com>
2020-07-29 14:50:28 +1200

    Checklist 2 complete

--- src/App.js
+++ src/App.js
@@ -6,20 +6,54 @@ class App extends Component{
 
   constructor(props){
     super(props)
-    this.notes = [
-      {
-        id:1,
-        text: 'Watch The Godfather 1',
-      },
-      {
-        id:2,
-        text: 'Watch The Godfather 2',
-      },
-      {
-        id:3,
-        text: 'Watch The Godfather 3',
-      },
-    ]
+
+    this.state = {
+      notes: [
+        {
+          id:1,
+          text: 'Watch The Godfather 1',
+          completed: false,
+        },
+        {
+          id:2,
+          text: 'Watch The Godfather 2',
+          completed: false,
+        },
+        {
+          id:3,
+          text: 'Watch The Godfather 3',
+          completed: false,
+        },
+      ],
+      noteInputValue : ''
+    }
+
+  }
+
+  handleNoteInputChange = (e)=>{
+    this.setState({noteInputValue:e.target.value})
+  }
+  handleAddButtonClick = (e)=>{
+    e.preventDefault()
+    var note = {
+      id:Date.now(),
+      text:this.state.noteInputValue
+    }
+    var newNotes = [note, ...this.state.notes]
+    this.setState({
+      notes:newNotes,
+      noteInputValue : ''
+    })
+  }
+  handleNoteDelete = (e)=>{
+    var noteIdToDelete = parseInt(e.target.id)
+    var notes = this.state.notes
+
+    var filteredNotes = notes.filter((item)=>{
+      return item.id != noteIdToDelete
+    })
+
+    this.setState({notes:filteredNotes})
   }
 
   render() {
@@ -31,19 +65,23 @@ class App extends Component{
               </div>
               <div className="add-note">
                   <label htmlFor="note-input"></label>
-                  <input type="text" placeholder=" Add Your Todo" className="note-input"></input>
-                  <i className="fas fa-plus"></i>
+                  <input type="text" placeholder=" Add Your Todo" className="note-input" value={this.state.noteInputValue} onChange={this.handleNoteInputChange}></input>
+                  <button type="submit" className="btn" onClick={this.handleAddButtonClick}><i className="fas fa-plus"></i></button>
               </div>
               <div className="notes">
                 {
-                  this.notes.map((note)=>{
+                  this.state.notes.map((note)=>{
                     return (
-                      <div className="note n1">
+                      <div className="note n1" key={note.id}>
                           <label className="checkbox">
                               <input type="checkbox" name="" id="checkbox"></input>
                           </label>
                           <div className="note-text">
                           {note.text}
+                          </div>
+                          <div class="icons">
+                            <i class="fas fa-edit"></i>
+                            <i id={note.id} class="fas fa-trash-alt" onClick={this.handleNoteDelete}></i>
                           </div>
                       </div>
                     )
